0eb1499278cb6aa25c84ad30307cc5a468842e41
Magdalena Powalisz <magdalenapowalisz@pc-40.home>
2017-07-20 13:48:11 +0200

    Addes Bootstrap (sass and dropdown.js

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,8 @@ var jsSources = [
     'components/scripts/rclick.js',
     'components/scripts/pixgrid.js',
     'components/scripts/tagline.js',
-    'components/scripts/template.js'
+    'components/scripts/template.js',
+    'node_modules/bootstrap-v4-dev/js/dist/dropdown.js'
 ];
 
 var sassSources = ['components/sass/style.scss'];
@@ -42,10 +43,14 @@ gulp.task('compass', function () {
         .pipe(gulp.dest('builds/development/css'))
 });
 
+gulp.task('boostrap', function() {
+    gulp.src()
+});
+
 gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
 
 gulp.task('watch', function() {
           gulp.watch(coffeeSources, ['coffee']);
           gulp.watch(jsSources, ['js']);
-            gulp.watch('components/sass/*.scss', ['compass']);
+            gulp.watch('components/sass/*.scss', ['compass'])
 });
